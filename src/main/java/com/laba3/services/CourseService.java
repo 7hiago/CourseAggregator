ca42f84b9647fd810639996afc722f5cc9504eea
Yevhenii Pavlenko <61028083+7hiago@users.noreply.github.com>
2022-06-15 21:49:33 +0300

    Issue #2. Refactor CourseService methodth

--- src/main/java/com/laba3/services/CourseService.java
+++ src/main/java/com/laba3/services/CourseService.java
@@ -1,10 +1,8 @@
 package com.laba3.services;
 
-import com.ctc.wstx.shaded.msv.relaxng_datatype.DatatypeException;
 import com.laba3.entities.Course;
 import com.laba3.exceptions.ApiInternalException;
 import com.laba3.exceptions.CourseNotFoundException;
-import com.laba3.exceptions.DateWrongParametersException;
 import com.laba3.utils.DateValidation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,7 +10,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-import java.time.format.DateTimeParseException;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 @Service
@@ -37,109 +35,46 @@ public class CourseService {
     @Cacheable(value = "courses-nacbank", key = "#currency")
     public Course getNacbankCurrentCourse(String currency) {
         logger.debug("execute getNacbankCurrentCourse method");
+        return getCourse(currency, nacBankApiServiceImpl.getCurrentCourse(currency));
+    }
+
+    @Cacheable(value = "courses-nacbank", key = "{#date, #currency }")
+    public Course getNacbankArchiveCourse(String date, String currency) {
+        logger.debug("execute getNacbankArchiveCourse method");
+        String validatedDate = dateValidation.validate(date, "yyyyMMdd");
+        return getCourse(currency, nacBankApiServiceImpl.getArchiveCourse(validatedDate, currency));
+    }
+
+    @Cacheable(value = "courses-privatbank", key = "#currency")
+    public Course getPrivatbankCurrentCourse(String currency) {
+        logger.debug("execute getPrivatbankCurrentCourse method");
+        return getCourse(currency, privatBankApiServiceImpl.getCurrentCourse(currency));
+    }
+
+    @Cacheable(value = "courses-privatbank", key = "{#date, #currency }")
+    public Course getPrivatbankArchiveCourse(String date, String currency) {
+        logger.debug("execute getPrivatbankArchiveCourse method");
+        String validatedDate = dateValidation.validate(date, "dd.MM.yyyy");
+        return getCourse(currency, privatBankApiServiceImpl.getArchiveCourse(validatedDate, currency));
+    }
+
+    @Cacheable(value = "courses-monobank", key = "#currency")
+    public Course getMonobankCurrentCourse(String currency) {
+        logger.debug("execute getMonobankCurrentCourse method");
+        return getCourse(currency, monoBankApiServiceImpl.getCurrentCourse(currency));
+    }
+
+    private Course getCourse(String currency, CompletableFuture<Course> bankApiServiceMethod) {
         Course course;
 
         try {
-            course = nacBankApiServiceImpl.getCurrentCourse(currency).get();
+            course = bankApiServiceMethod.get();
             if(course == null) {
                 logger.warn(currency + " currency is not available in this bank");
                 throw new CourseNotFoundException(currency + " currency is not available in this bank");
             }
         } catch (InterruptedException | ExecutionException e) {
             logger.debug(e.getMessage());
-            throw new ApiInternalException(e.getMessage());
-        }
-
-        return course;
-    }
-
-    @Cacheable(value = "courses-nacbank", key = "{#date, #currency }")
-    public Course getNacbankArchiveCourse(String date, String currency) {
-        logger.debug("execute getNacbankArchiveCourse method");
-        Course course;
-
-        try {
-            date = dateValidation.validate(date, "yyyyMMdd");
-        } catch (DateTimeParseException e) {
-            throw new DateWrongParametersException(date + " date format is not available. Use dd.MM.yyyy format!");
-        } catch (DatatypeException e) {
-            throw new DateWrongParametersException(date + " wrong date. Date must be before today!");
-        }
-
-        try {
-            course = nacBankApiServiceImpl.getArchiveCourse(date, currency).get();
-            if(course == null) {
-                logger.warn(currency + " currency is not available in this bank");
-                throw new CourseNotFoundException(currency + " currency is not available in this bank");
-            }
-        } catch (InterruptedException | ExecutionException e) {
-            logger.error(e.getMessage());
-            throw new ApiInternalException(e.getMessage());
-
-        }
-
-        return course;
-    }
-
-    @Cacheable(value = "courses-privatbank", key = "#currency")
-    public Course getPrivatbankCurrentCourse(String currency) {
-        logger.debug("execute getPrivatbankCurrentCourse method");
-        Course course;
-
-        try {
-            course = privatBankApiServiceImpl.getCurrentCourse(currency).get();
-            if(course == null) {
-                logger.warn(currency + " currency is not available in this bank");
-                throw new CourseNotFoundException(currency + " currency is not available in this bank");
-            }
-        } catch (InterruptedException | ExecutionException e) {
-            logger.error(e.getMessage());
-            throw new ApiInternalException(e.getMessage());
-        }
-
-        return course;
-    }
-
-    @Cacheable(value = "courses-privatbank", key = "{#date, #currency }")
-    public Course getPrivatbankArchiveCourse(String date, String currency) {
-        logger.debug("execute getPrivatbankArchiveCourse method");
-        Course course;
-
-        try {
-            date = dateValidation.validate(date, "dd.MM.yyyy");
-        } catch (DateTimeParseException e) {
-            throw new DateWrongParametersException(date + " date format is not available. Use dd.MM.yyyy format!");
-        } catch (DatatypeException e) {
-            throw new DateWrongParametersException(date + " wrong date. Date must be before today!");
-        }
-
-        try {
-            course = privatBankApiServiceImpl.getArchiveCourse(date, currency).get();
-            if(course == null) {
-                logger.warn(currency + " currency is not available in this bank");
-                throw new CourseNotFoundException(currency + " currency is not available in this bank");
-            }
-        } catch (InterruptedException | ExecutionException e) {
-            logger.error(e.getMessage());
-            throw new ApiInternalException(e.getMessage());
-        }
-
-        return course;
-    }
-
-    @Cacheable(value = "courses-monobank", key = "#currency")
-    public Course getMonobankCurrentCourse(String currency) {
-        logger.debug("execute getMonobankCurrentCourse method");
-        Course course;
-
-        try {
-            course = monoBankApiServiceImpl.getCurrentCourse(currency).get();
-            if(course == null) {
-                logger.warn(currency + " currency is not available in this bank");
-                throw new CourseNotFoundException(currency + " currency is not available in this bank");
-            }
-        } catch (InterruptedException | ExecutionException e) {
-            logger.error(e.getMessage());
             throw new ApiInternalException(e.getMessage());
         }
 
--- src/main/java/com/laba3/utils/DateValidation.java
+++ src/main/java/com/laba3/utils/DateValidation.java
@@ -1,25 +1,30 @@
 package com.laba3.utils;
 
-import com.ctc.wstx.shaded.msv.relaxng_datatype.DatatypeException;
+import com.laba3.exceptions.DateWrongParametersException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 @Component
 public class DateValidation {
     private static final Logger logger = LoggerFactory.getLogger(DateValidation.class);
 
-    public String validate(String inputDate, String requireOutputFormat) throws DatatypeException {
+    public String validate(String inputDate, String requireOutputFormat) {
         String date;
         logger.debug("executing validate method and get date: " + inputDate);
-        LocalDate localDate = LocalDate.parse(inputDate, DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        if(localDate.isAfter(LocalDate.now()) || localDate.isEqual(LocalDate.now())) {
-            throw new DatatypeException();
+        try {
+            LocalDate localDate = LocalDate.parse(inputDate, DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+            if (localDate.isAfter(LocalDate.now()) || localDate.isEqual(LocalDate.now())) {
+                throw new DateWrongParametersException(inputDate + " wrong date. Date must be before today!");
+            }
+            date = LocalDate.parse(inputDate, DateTimeFormatter.ofPattern("dd.MM.yyyy")).format(DateTimeFormatter.ofPattern(requireOutputFormat));
+        } catch (DateTimeParseException e) {
+            throw new DateWrongParametersException(inputDate + " date format is not available. Use dd.MM.yyyy format!");
         }
-        date = LocalDate.parse(inputDate, DateTimeFormatter.ofPattern("dd.MM.yyyy")).format(DateTimeFormatter.ofPattern(requireOutputFormat));
         logger.debug("executed validate method and get date: " + date);
         return date;
     }
